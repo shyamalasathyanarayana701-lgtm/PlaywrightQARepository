43554a86d5b13ce48dbd7dea4486065244308b38
shyamalasathyanarayana701-lgtm <shyamalasathyanarayana701@gmail.com>
2026-03-29 12:10:58 +0530

    W3School Launch Testcase

--- SampleProject/tests/sample.spec.js
+++ SampleProject/tests/sample.spec.js
@@ -3,4 +3,9 @@ const {test, expect} = require('@playwright/test')
 test("Launch Application and Validate", async ({page})=>{
     await page.goto("https://www.w3schools.com/")
     await page.waitForTimeout(3000)
+    const title = await page.title()
+    console.log("Title :"+title);
+    const url = await page.url()
+    console.log("URL of Application :"+url);
+   
 })
