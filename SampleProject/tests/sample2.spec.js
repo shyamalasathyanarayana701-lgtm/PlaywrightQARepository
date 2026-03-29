8530b2298c94cc48bdacdfca6de8dd8c7ff71791
shyamalasathyanarayana701-lgtm <shyamalasathyanarayana701@gmail.com>
2026-03-29 20:47:32 +0530

    sample3 w3 school launch test case

---
+++ SampleProject/tests/sample2.spec.js
@@ -0,0 +1,7 @@
+const {test, expect} = require('@playwright/test')
+ 
+test("Launch Application and Validate", async ({page})=>{
+    await page.goto("https://www.w3schools.com/")
+    await page.waitForTimeout(3000)
+})
+ 
